52878b370985a899286def8504c9f6d47c8a3a1c
shreytheshreyas <59922045+shreytheshreyas@users.noreply.github.com>
2023-02-07 23:53:10 +0800

    Made some changes to client code implementation of internal comms

--- Internal-Communications/beetle_code_simple_version/beetle_code_simple_version.ino
+++ Internal-Communications/beetle_code_simple_version/beetle_code_simple_version.ino
@@ -1,6 +1,3 @@
-#include "CRC.h"
-#include "CRC8.h"
-
 #define DATA_RATE 115200 //Defining datarate to be 115200 baud
 #define PACKET_SIZE 20 //Defining packet size to be 20 bytes
 
@@ -10,23 +7,47 @@
 
 #define ACK 65 //The letter 'A' corresponds to the ASCII code 65
 	       //
-uint_8 dataPacket[PACKET_SIZE];
-uint_8 sequenceNumber = 0;
-
+byte dataPacket[PACKET_SIZE];
+int sequenceNumber = 0;
+bool condition = false;
 void create_data_packet(float data) {
 	//TODO: Need to define code over here
 }
 
 void create_handshake_packet() {
-
+ 
 }
 
+void send_ack_packet() {
+
+  Serial.write("A");
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+//  Serial.write(0);
+}
 void handshake_mechanism(bool isHandShakeComplete) {
-	int receivedPacket = Serial.read();
+	int receivedData = Serial.read();
 	bool completionStatus = false;
 
 	//If received packet is 'S', create ACK packet and send to the relay node
-	if(relayNodeData == 83) 
+	if(receivedData == 83) 
 		Serial.write(ACK);
 	
 	//If received packet is 'A' set complettionStatus to True
@@ -34,18 +55,30 @@ void handshake_mechanism(bool isHandShakeComplete) {
 		completionStatus = true;
 	
 	
-	return completionStatus	
+	return completionStatus;
 }
 
 void setup(void) {
-
+  Serial.begin(115200);
 }
 
 void loop(void) {
-	bool isHandshakeComplete = false;
-	if (!isHandshakeComplete)
-		isHandshakeComplete = handshake_mechanism()
-	
-	if (isHandshakeComplete) 
-		//TODO: Data-Transfer can occur
+//	bool isHandshakeComplete = false;
+//	if (!isHandshakeComplete)
+//		isHandshakeComplete = handshake_mechanism()
+//	
+//	if (isHandshakeComplete) 
+//		//TODO: Data-Transfer can occur
+//  dataPacket[0] = (byte) 65;
+//  for (int i = 1; i < PACKET_SIZE; i++) {
+//    dataPacket[i] = (byte) 65;
+//  }
+
+//  Serial.write(dataPacket,sizeof(dataPacket));
+
+//  if(!condition)
+    send_ack_packet();
+    delay(1000);
+//  condition = !condition;
+  condition = true;
 }
