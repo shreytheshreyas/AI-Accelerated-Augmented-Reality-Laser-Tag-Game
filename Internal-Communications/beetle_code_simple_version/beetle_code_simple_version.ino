56b5419f3a22e05cb472104102dec24e3ac390e5
shreytheshreyas <59922045+shreytheshreyas@users.noreply.github.com>
2023-02-13 14:31:43 +0800

    Added classes for packer protocol implementaion

--- Internal-Communications/beetle_code_simple_version/beetle_code_simple_version.ino
+++ Internal-Communications/beetle_code_simple_version/beetle_code_simple_version.ino
@@ -1,26 +1,131 @@
 #define DATA_RATE 115200 //Defining datarate to be 115200 baud
 #define PACKET_SIZE 20 //Defining packet size to be 20 bytes
 
-#define BEETLE_ID 0 //Need to change this for each and every beetle
+#define BEETLE_DEVICE 0 //Need to change this for each and every beetle
+
+//Packet Indices 
+#define BEETLE_ID 0
 #define SEQUENCE_NUMBER 1
 #define PACKET_TYPE 2
 
-#define ACK 65 //The letter 'A' corresponds to the ASCII code 65
-	       //
+#define ACK 'A' //The letter 'A' corresponds to the ASCII code 65
+#define SYN 'S'
+
+class Packet {
+  private:
+    float imu_gx;
+    float imu_gy;
+    float imu_gz;
+    float imu_ax;
+    float imu_ay;
+    float imu_az;
+    
+    uint8_t packet_message[20];
+  public:
+    void getImuData();
+    void getGunData();
+    void getVestData();
+    
+    Packet() {
+      getImuData();
+      getGunData();
+      getVestData();
+      
+      packet_message[BEETLE_ID] = BEETLE_DEVICE;
+      packet_message[SEQUENCE_NUMBER] = BEETLE_DEVICE;
+      for(int i = 0; i < PACKET_SIZE; i++) 
+        packet_message[i] = 0;
+    }
+    
+    Packet(uint8_t packetType) {
+      packet_message[BEET
+};
+
+
+class ImuDataPacket: public Packet {
+  private: 
+    float gx;
+    float gy;
+    float gz;
+    float ax;
+    float ay;
+    float az;
+    
+  public:
+    void getImuData();
+    ImuDataPacket() {
+      this.getImuData();
+      this.Packet('I')
+    }
+};
+
+class GunDataPacket: public Packet {
+  private:
+    int address;
+    int command;
+
+  public:
+    void getGunData();
+    GunDataPacket() {
+      this.getGunData();
+      this.  
+    }
+};
+
+class VestDataPacket: public Packet {
+  private:
+    int address;
+    int command;
+
+    public:
+      void 
+ };
+ class 
+class ReysProtocol {
+  private:
+  bool isHandshake;
+//  Packet dataPacket;
+
+  public:
+
+  ReysProtocol() {
+  isHandshake = false;
+//  dataPacket = new Packet() 
+  }
+
+  void start() {
+      if(isHandshake) {
+        Serial.println("Handshake with relay node completed");
+      }  
+
+      else {
+        byte relayNodePacket = Serial.read();
+
+        if(relayNodePacket == SYN)
+          Serial.write(ACK);
+
+        if(relayNodePacket == ACK)
+          isHandshake = true;
+      }  
+  }
+
+};
+
 byte dataPacket[PACKET_SIZE];
 int sequenceNumber = 0;
-bool condition = false;
+ReysProtocol* protocol;
+
+//dummy variables 
+//bool condition = false;
+
 void create_data_packet(float data) {
 	//TODO: Need to define code over here
 }
 
-void create_handshake_packet() {
- 
-}
 
-void send_ack_packet() {
+void send_dummy_packet() {
 
-  Serial.write("A");
+  Serial.write("100");
 //  Serial.write(0);
 //  Serial.write(0);
 //  Serial.write(0);
@@ -42,43 +147,29 @@ void send_ack_packet() {
 //  Serial.write(0);
 //  Serial.write(0);
 }
-void handshake_mechanism(bool isHandShakeComplete) {
-	int receivedData = Serial.read();
-	bool completionStatus = false;
 
-	//If received packet is 'S', create ACK packet and send to the relay node
-	if(receivedData == 83) 
-		Serial.write(ACK);
-	
-	//If received packet is 'A' set complettionStatus to True
-	if(receivedData == 65)
-		completionStatus = true;
-	
-	
-	return completionStatus;
-}
+//void handshake_mechanism(bool isHandShakeComplete) {
+//	int receivedData = Serial.read();
+//	bool completionStatus = false;
+//
+//	//If received packet is 'S', create ACK packet and send to the relay node
+//	if(receivedData == 83) 
+//		Serial.write(ACK);
+//	
+//	//If received packet is 'A' set complettionStatus to True
+//	if(receivedData == 65)
+//		completionStatus = true;
+//	
+//	
+//	return completionStatus;
+//}
 
 void setup(void) {
   Serial.begin(115200);
+  protocol = new ReysProtocol();
 }
 
 void loop(void) {
-//	bool isHandshakeComplete = false;
-//	if (!isHandshakeComplete)
-//		isHandshakeComplete = handshake_mechanism()
-//	
-//	if (isHandshakeComplete) 
-//		//TODO: Data-Transfer can occur
-//  dataPacket[0] = (byte) 65;
-//  for (int i = 1; i < PACKET_SIZE; i++) {
-//    dataPacket[i] = (byte) 65;
-//  }
-
-//  Serial.write(dataPacket,sizeof(dataPacket));
-
-//  if(!condition)
-    send_ack_packet();
-    delay(1000);
-//  condition = !condition;
-  condition = true;
+    protocol->start();
+//    delay(1000);
 }
